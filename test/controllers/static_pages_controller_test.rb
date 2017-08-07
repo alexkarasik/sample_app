5188d2e07e0b4bbfede1bcba876c431034a4cbfc
kinkorea <alexkarasik89@gmail.com>
2017-08-06 23:02:39 -0400

    Created application.html.erb file as a layout in views, then applied this layour to the static_pages views, deleting redundant code and making for less duplication. Checked with tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup do
+  def setup 
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
